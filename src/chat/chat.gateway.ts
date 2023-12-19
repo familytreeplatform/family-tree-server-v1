7cb8119f303b5dbcf7427745e0b6511cca10ed30
Christopher Pam <chrystopam@gmail.com>
2023-12-19 22:11:22 +0100

    [feat]: add cors to websocket

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -11,7 +11,12 @@ import { ObjectId } from 'mongoose';
 import { ChatService } from './chat.service';
 import { MessageDto } from './dto';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+  transports: ['websocket'],
+})
 export class ChatGateway implements OnGatewayConnection {
   private store = new Map<ObjectId, string>();
 
