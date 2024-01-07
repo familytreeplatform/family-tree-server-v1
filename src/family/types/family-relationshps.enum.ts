82c0f4c342fd8d23d938ce8d88231d200eab9208
MhdSadd <mhdsadd3@gmail.com>
2024-01-07 21:33:46 +0100

    chore: include 4th gen. (great-great grand) realationships

--- src/family/types/family-relationshps.enum.ts
+++ src/family/types/family-relationshps.enum.ts
@@ -1,12 +1,16 @@
 export enum FamilyRelationshipsEnum {
   Root = 'root',
   Spouse = 'spouse',
+  Brother = 'brother',
+  Sister = 'sister',
   Son = 'son',
   Daughter = 'daughter',
   Grandson = 'grandson',
   Granddaughter = 'granddaughter',
   Greatgrandson = 'greatgrandson',
   Greatgranddaughter = 'greatgranddaughter',
+  Greatgreatgrandson = 'greatgreatgrandson',
+  Greatgreatgranddaughter = 'greatgreatgranddaughter',
 }
 
 export enum FamilyTypeEnum {
@@ -17,6 +21,8 @@ export enum FamilyTypeEnum {
 export const zeroToFirstGenerationFamilyRelations = [
   FamilyRelationshipsEnum.Root,
   FamilyRelationshipsEnum.Spouse,
+  FamilyRelationshipsEnum.Brother,
+  FamilyRelationshipsEnum.Sister,
   FamilyRelationshipsEnum.Son,
   FamilyRelationshipsEnum.Daughter,
 ];
